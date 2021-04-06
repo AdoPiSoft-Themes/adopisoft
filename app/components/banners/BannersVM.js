cb3a1b4c76e53c83789b7bc0e8c3788809da959c
Adones Pitogo <adonesp@pop-os-x399>
2021-04-07 07:59:41 +0800

    clean up

--- app/components/banners/BannersVM.js
+++ app/components/banners/BannersVM.js
@@ -1,4 +1,4 @@
-define(['../../utils/config', 'jquery', '../../bindings/imageSlider'], function (config) {
+define(['../../utils/config', '../../bindings/imageSlider'], function (config) {
   var banner_field = config.findField('banners', 'banners');
   return function () {
     this.img_urls = banner_field.value;
