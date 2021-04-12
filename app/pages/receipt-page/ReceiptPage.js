d6395e6b3ef46f98805a57ccd93ff5515fffcd45
Adones Pitogo <adonesp@Adoness-MacBook-Pro.local>
2021-04-13 04:30:30 +0800

    concat new session to sessions list

--- app/pages/receipt-page/ReceiptPage.js
+++ app/pages/receipt-page/ReceiptPage.js
@@ -3,8 +3,9 @@ define([
   'rootVM',
   'app/observables/receipt',
   'app/services/sessions',
+  'app/observables/session',
   'app/services/http'
-], function(ko, rootVM, receipt, sessions, http) {
+], function(ko, rootVM, receipt, sessions, Session, http) {
   ko.components.register('receipt-page', {
     viewModel: function ReceiptPage() {
       this.receipt = receipt;
@@ -15,7 +16,9 @@ define([
         rootVM.showingSessionsTable(false);
       };
       this.connect = function() {
-        http.startSession(receipt.sessionId(), function(err) {
+        http.startSession(receipt.sessionId(), function(err, data) {
+          var s = new Session(data);
+          sessions.get().push(s);
           rootVM.navigate('home-page');
         });
       };
