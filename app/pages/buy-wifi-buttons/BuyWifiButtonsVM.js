505f7373f9bebcc539c672e5e79157a09033d71e
Adones Pitogo <adonesp@Adoness-MacBook-Pro.local>
2021-04-13 06:12:35 +0800

    auto select rate type

--- app/pages/buy-wifi-buttons/BuyWifiButtonsVM.js
+++ app/pages/buy-wifi-buttons/BuyWifiButtonsVM.js
@@ -5,18 +5,28 @@ define([
   'rootVM'
 ], function (ko, rates, payment, rootVM) {
   return function () {
-    this.sampleText = ko.observable('txt-1');
-    this.rates = rates;
-    this.loading = ko.observable(false);
-    this.koDescendantsComplete = function () {
+    var self = this;
+    self.sampleText = ko.observable('txt-1');
+    self.rates = rates;
+    self.loading = ko.observable(false);
+    self.koDescendantsComplete = function () {
       rootVM.showingStatusNav(true);
       rootVM.showingBanners(true);
       rootVM.showingSessionsTable(true);
+
+      if (!rates.time_or_data_rates()) {
+        if (rates.time_rates() && !rates.data_rates()) self.selectRate('time');
+        if (!rates.time_rates() && rates.data_rates()) self.selectRate('data');
+      } else {
+        self.selectRate('time_or_data');
+      }
+
     };
-    this.selectRate = function (type) {
-      this.loading(true);
+    self.selectRate = function (type) {
+      self.loading(true);
       payment.rateType(type);
       rootVM.navigate('select-coinslot-page');
     };
+
   };
 });
