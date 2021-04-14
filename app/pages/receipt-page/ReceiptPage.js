ed06cd8978498727f1583f498f37e5c63a7540c9
Adones Pitogo <adonesp@Adoness-MacBook-Pro.local>
2021-04-14 16:57:22 +0800

    receipt page lazy load

--- app/pages/receipt-page/ReceiptPage.js
+++ app/pages/receipt-page/ReceiptPage.js
@@ -1,35 +1,8 @@
 define([
-  'knockout',
-  'rootVM',
-  'app/observables/receipt',
-  'app/services/sessions',
-  'app/observables/session',
-  'app/services/http',
-  'app/components/voucher-form/VoucherForm',
-  'app/bindings/clipboard'
-], function(ko, rootVM, receipt, sessions, Session, http) {
+  'knockout'
+], function(ko) {
   ko.components.register('receipt-page', {
-    viewModel: function ReceiptPage() {
-      var self = this;
-      self.receipt = receipt;
-      self.sessions = sessions;
-      self.copied = ko.observable(false);
-      self.koDescendantsComplete = function () {
-        rootVM.showingStatusNav(false);
-        rootVM.showingBanners(false);
-        rootVM.showingSessionsTable(false);
-      };
-      self.afterCopy = function() {
-        self.copied(true);
-      };
-      self.connect = function() {
-        http.startSession(receipt.sessionId(), function(err, data) {
-          var s = new Session(data);
-          sessions.get().push(s);
-          rootVM.navigate('home-page');
-        });
-      };
-    },
+    viewModel: {require: 'app/pages/receipt-page/ReceiptPageVM'},
     template: {require: 'text!app/pages/receipt-page/receipt-page.html'}
   });
 });
---
+++ app/pages/receipt-page/ReceiptPageVM.js
@@ -0,0 +1,32 @@
+define([
+  'knockout',
+  'rootVM',
+  'app/observables/receipt',
+  'app/services/sessions',
+  'app/observables/session',
+  'app/services/http',
+  'app/components/voucher-form/VoucherForm',
+  'app/bindings/clipboard'
+], function(ko, rootVM, receipt, sessions, Session, http) {
+  return function ReceiptPage() {
+    var self = this;
+    self.receipt = receipt;
+    self.sessions = sessions;
+    self.copied = ko.observable(false);
+    self.koDescendantsComplete = function () {
+      rootVM.showingStatusNav(false);
+      rootVM.showingBanners(false);
+      rootVM.showingSessionsTable(false);
+    };
+    self.afterCopy = function() {
+      self.copied(true);
+    };
+    self.connect = function() {
+      http.startSession(receipt.sessionId(), function(err, data) {
+        var s = new Session(data);
+        sessions.get().push(s);
+        rootVM.navigate('home-page');
+      });
+    };
+  };
+});
