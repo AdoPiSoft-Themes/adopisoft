15f0c7a381a278cc61377fcdf79ec7a3f5481dcd
Adones Pitogo <adonesp@pop-os-x399>
2021-04-15 05:07:52 +0800

    show sessions im more buttons

--- app/pages/more-buttons/MoreButtons.js
+++ app/pages/more-buttons/MoreButtons.js
@@ -1,10 +1,16 @@
 define([
   'knockout',
+  'rootVM',
   'app/services/config'
-], function(ko, config) {
+], function(ko, rootVM, config) {
   ko.components.register('more-buttons', {
     viewModel: function () {
       this.config = config;
+      this.koDescendantsComplete = function () {
+        rootVM.showingStatusNav(true);
+        rootVM.showingBanners(true);
+        rootVM.showingSessionsTable(true);
+      };
     },
     template: {require: 'text!app/pages/more-buttons/more-buttons.html'}
   });
