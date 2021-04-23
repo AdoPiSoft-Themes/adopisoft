dfc5e1b6f65aac33bb15076437ecc9dc7c7c12bf
Adones Pitogo <adonesp@Adoness-MacBook-Pro.local>
2021-04-23 19:42:28 +0800

    dynamic wifi icon

--- app/components/status-nav/connection-status/ConnectionStatusComponent.js
+++ app/components/status-nav/connection-status/ConnectionStatusComponent.js
@@ -1,9 +1,13 @@
 define([
   'knockout',
-  'sessions'
-], function(ko, sessions) {
+  'sessions',
+  'app/services/config'
+], function(ko, sessions, config) {
   ko.components.register('connection-status', {
     viewModel: function() {
+      this.connectedIcon = config.findField('wifi_icons', 'wifi_connected_icon');
+      this.disconnectedIcon = config.findField('wifi_icons', 'wifi_disconnected_icon');
+      console.log(this);
       this.connected = ko.pureComputed(function () {
         return sessions.hasRunning();
       });
