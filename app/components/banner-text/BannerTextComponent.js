56febc574ddde54f51e9c8b80d6b16f08deae7fc
root <root@app.local>
2021-05-01 02:38:30 +0800

    remove extra files

--- app/components/banner-text/BannerTextComponent.js
+++
@@ -1,8 +0,0 @@
-define([
-  'knockout'
-], function (ko) {
-  ko.components.register('banner-text', {
-    viewModel: {require: 'app/components/banner-text/BannerTextVM'},
-    template: {require: 'text!app/components/banner-text/banner-text.html'}
-  });
-});
--- app/components/banner-text/BannerTextVM.js
+++
@@ -1,7 +0,0 @@
-define([
-  'app/services/config'
-], function(config) {
-  return function () {
-    this.bannerText = config.findField('page_properties', 'banner_text');
-  };
-});
