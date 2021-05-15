f2b0cd2c552dc0e75043369b16d3e6a03eeebaa7
Arnel Lenteria <arnel.lenteria@gmail.com>
2021-05-15 15:04:31 +0800

    merge master

--- app/init.js
+++ app/init.js
@@ -1,7 +1,8 @@
 define([
   'app/observables/device',
-  'socket'
-], function (device, socket) {
+  'socket',
+  'app/services/plugin_assets'
+], function (device, socket, assets) {
   return function init(cb) {
     device.fetch(function (d) {
       var socket_instance = socket(d);
@@ -10,6 +11,25 @@ define([
           return socket_instance;
         }
       };
+
+      // insert plugin assets
+      for(var i = 0; i < assets.length; i++) {
+        var p = assets[i];
+        for (var x = 0; x < p.assets.scripts.length; x++) {
+          var js_src = p.assets.scripts[x];
+          var s = document.createElement('script');
+          s.src = js_src;
+          document.getElementsByTagName('head')[0].appendChild(s);
+        }
+        for (var y = 0; y < p.assets.styles.length; y++) {
+          var css_src = p.assets.styles[y];
+          var link = document.createElement('link');
+          link.href = css_src;
+          link.type = 'text/css';
+          link.rel = 'stylesheet';
+          document.getElementsByTagName('head')[0].appendChild(link);
+        }
+      }
 
       cb();
     });
