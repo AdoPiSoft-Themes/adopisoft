7245295d6d68bc38ce44bb79621fe3b155f36335
root <root@app.local>
2021-09-02 00:26:34 +0800

    clean up pages

--- app/pages/home/HomePage.js
+++ app/pages/home/HomePage.js
@@ -1,8 +1,21 @@
 define([
-  'knockout'
-], function (ko) {
+  'knockout',
+  'rootVM',
+  'app/components/start-pause-buttons/StartPauseButtonsComponent',
+  'app/components/buy-wifi-or-eload/BuyWifiOrEloadComponent',
+  'app/components/voucher-form/VoucherForm',
+  'app/components/device-info/DeviceInfo',
+  'app/components/internet-status/InternetStatus'
+
+], function (ko, rootVM) {
   ko.components.register('home-page', {
-    viewModel: { require: 'app/pages/home/HomePageVM' },
+    viewModel: function () {
+      this.koDescendantsComplete = function() {
+        rootVM.showingBanners(true);
+        rootVM.showingSessionsTable(true);
+        rootVM.showingStatusNav(true);
+      };
+    },
     template: { require: 'text!app/pages/home/home-page.html' }
   });
 });
