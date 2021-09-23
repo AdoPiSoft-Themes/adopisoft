e54b5ba3a2e1d7ca020eef5e1bf7a657ef371915
Adones Pitogo <adonesp@Adoness-MacBook-Pro.local>
2021-09-23 09:58:21 +0800

    translate popup banner

--- app/components/popup-banner/PopupBanner.js
+++ app/components/popup-banner/PopupBanner.js
@@ -4,13 +4,13 @@ define([
 ], function (ko, config) {
 
   var tpl = '<div class="modal-header text-center">' +
-    '<h2>' + 'Announcement' + '</h2>' +
+    '<h2>' + '<span data-bind="translate: \'ANNOUNCEMENT\'"></span>' + '</h2>' +
     '</div>' +
     '<div class="modal-body">' +
     config.findField('banners', 'popup_banner') +
     '</div>' +
     '<div class="modal-footer"><button class="btn btn-default" data-bind="click: close">'+
-    'Close' +
+    '<span data-bind="translate: \'CANCEL\'"></span>' +
     '</button></div>';
 
   ko.components.register('popup-banner', {
