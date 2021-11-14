22a6307d1ba0cfe694876ed8f443b5911af2a273
Adones Pitogo <pitogo.adones@gmail.com>
2021-11-14 16:04:06 +0800

    updated new api urls

--- app/services/http.js
+++ app/services/http.js
@@ -46,19 +46,19 @@ define([
       toast.error(message);
     };
     http.fetchSessions = function (cb) {
-      http.get('/client/sessions', cb);
+      http.get('/portal/sessions', cb);
     };
     http.startSession = function(s_id, cb) {
-      http.post('/client/sessions/' + s_id + '/start', cb);
+      http.post('/portal/sessions/' + s_id + '/start', cb);
     };
     http.pauseSession = function (s_id, cb) {
-      http.post('/client/sessions/' + s_id + '/pause', cb);
+      http.post('/portal/sessions/' + s_id + '/pause', cb);
     };
     http.fetchRates = function (cb) {
       http.get('/settings/timer/rates', cb);
     };
     http.fetchCoinslots = function (cb) {
-      http.get('/client/coinslots', cb);
+      http.get('/portal/coinslots', cb);
     };
 
     http.queForPayment = function (opts, cb) {
@@ -71,77 +71,77 @@ define([
         product_keyword: opts.product_keyword
       };
 
-      http.post('/client/payments/que', data, cb);
+      http.post('/portal/payments/que', data, cb);
     };
 
     http.getDevice = function (cb) {
-      http.get('/client/device', cb);
+      http.get('/portal/device', cb);
     };
     http.currentPaymentQue = function (cb) {
-      http.get('/client/payments/current', cb);
+      http.get('/portal/payments/current', cb);
     };
     http.timerConfig = function (cb) {
       http.get('/settings/timer/config', cb);
     };
     http.donePayment = function (coinslot_id, cb) {
-      http.post('/client/payments/done', {coinslot_id: coinslot_id}, cb);
+      http.post('/portal/payments/done', {coinslot_id: coinslot_id}, cb);
     };
     http.activateVoucher = function(code, cb) {
-      http.post('/vouchers/activate', {code: code}, cb);
+      http.post('/portal/vouchers/activate', {code: code}, cb);
     };
     http.getVouchers = function(cb) {
       http.get('/vouchers/list', cb);
     };
 
     http.getCurrentUser = function(cb) {
-      http.get('/user/me', cb);
+      http.get('/u/api/user/me', cb);
     };
 
     // Eload
     http.checkEloadAvailability = function(cb) {
-      http.get('/client/eload/is-available', cb);
+      http.get('/portal/eload/is-available', cb);
     };
 
     http.getEloadClientData = function(acc_number, cb) {
-      http.get('/client/eload/customer-data?account_number=' + acc_number, cb);
+      http.get('/portal/eload/customer-data?account_number=' + acc_number, cb);
     };
 
     http.getEloadProviders = function(acc_number, cb) {
-      http.get('/client/eload/providers?account_number=' + acc_number, cb);
+      http.get('/portal/eload/providers?account_number=' + acc_number, cb);
     };
 
     http.getEloadPromos = function(opts, cb) {
       var params = Object.keys(opts).map(function (k) {
         return encodeURIComponent(k) + '=' + encodeURIComponent(opts[k]);
       }).join('&');
-      http.get('/client/eload/promos?' + params, cb);
+      http.get('/portal/eload/promos?' + params, cb);
     };
 
     http.getRegularDenoms = function(provider_id, cb) {
-      http.get('/client/eload/regular-denoms?provider_id=' + provider_id, cb);
+      http.get('/portal/eload/regular-denoms?provider_id=' + provider_id, cb);
     };
 
     http.activateEloadVoucher = function(account_number, code, cb) {
-      http.post('/client/eload/activate-voucher', {
+      http.post('/portal/eload/activate-voucher', {
         account_number: account_number,
         code: code
       }, cb);
     };
 
     http.checkEloadProvider = function(id, product_keyword, cb) {
-      http.post('/client/eload/check-provider', { id: id, product_keyword: product_keyword }, cb);
+      http.post('/portal/eload/check-provider', { id: id, product_keyword: product_keyword }, cb);
     };
 
     http.getRelatedTxn = function(account_number, product_keyword, cb) {
-      http.get('/client/eload/related-txn?account_number=' + account_number + '&product_keyword=' + product_keyword, cb);
+      http.get('/portal/eload/related-txn?account_number=' + account_number + '&product_keyword=' + product_keyword, cb);
     };
 
     http.customerPurchase = function(opts, cb) {
-      http.post('/customer/purchase', opts, cb);
+      http.post('/u/api/customer/purchase', opts, cb);
     };
 
     http.purchaseLoad = function(account_number, provider_id, product_keyword, voucher_id, cb) {
-      http.post('/client/eload/purchase', {
+      http.post('/portal/eload/purchase', {
         account_number: account_number,
         provider_id: provider_id,
         product_keyword: product_keyword,
@@ -150,13 +150,13 @@ define([
     };
 
     http.systemNotifications = function (cb) {
-      http.get('/client/system/notifications', cb);
+      http.get('/portal/system/notifications', cb);
     };
 
     // /eload
 
     http.logoutCustomer = function() {
-      http.post('/customer/logout');
+      http.post('/u/api/customer/logout');
     };
   }
 
