a6d722ab4d86762dfd973d56da100ba04593366c
Jonel Lenteria <jlenteria98@gmail.com>
2022-04-04 10:56:22 +0800

    validate passcode for only alpha numeric characters

--- app/components/passcode-modal/PasscodeModal.js
+++ app/components/passcode-modal/PasscodeModal.js
@@ -13,18 +13,27 @@ define([
     self.passcode = ko.observable(device.passcode());
     self.submitting = ko.observable(false);
     self.submitPasscode = function () {
-      self.submitting(true)
-      http.submitPasscode(self.passcode(), function(err) {
-        self.submitting(false)
-        if (err) {
-          http.catchError(err)
-        } else {
-          toast.success('Passcode successfully set');
-          device.passcode(self.passcode());
-          params.close();
-        }
-      });
+      if(self.validPasscode()){
+        self.submitting(true)
+        http.submitPasscode(self.passcode(), function(err) {
+          self.submitting(false)
+          if (err) {
+            http.catchError(err)
+          } else {
+            toast.success('Passcode successfully set');
+            device.passcode(self.passcode());
+            params.close();
+          }
+        });
+      }else{
+        toast.error("Error", "Combination of letters and numbers required.")
+      }
     };
+
+    self.validPasscode = function() {
+      const alpha_numberic_regex = /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$/
+      return alpha_numberic_regex.test(self.passcode())
+    }
 
     self.koDescendantsComplete = function () {
       document.getElementById('passcode').focus();
