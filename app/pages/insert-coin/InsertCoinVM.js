aaba0f20755c909dadea0a87d6cde60723295a44
Jonel Lenteria <jlenteria98@gmail.com>
2022-10-20 10:34:41 +0800

    fix

--- app/pages/insert-coin/InsertCoinVM.js
+++ app/pages/insert-coin/InsertCoinVM.js
@@ -61,7 +61,7 @@ define([
       http.currentPaymentQue(function (err, data) {
         if (!err) {
           self.onPaymentReceived(data);
-          self.coinslotInfo(data.coinslot_id)
+          self.coinslotInfo(data.coinslot_id, data.type)
         }
         fetch_timeout = setTimeout(function () {
           self.fetch();
@@ -100,8 +100,8 @@ define([
       }
     };
 
-    self.coinslotInfo = function (coinslot_id) {
-      http.fetchCoinslots(function(err, data) {
+    self.coinslotInfo = function (coinslot_id, rate_type) {
+      http.fetchCoinslots(rate_type, function(err, data) {
         const activeCoinslot = data.filter(d => d.id === coinslot_id)[0]
         if(activeCoinslot) {
           self.coinslot_alias('Coinslot: ' + activeCoinslot.alias.toUpperCase())
