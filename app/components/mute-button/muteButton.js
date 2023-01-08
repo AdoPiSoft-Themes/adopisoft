5caefeaf912110933d9a55e0396bbc071355e402
Arnel Lenteria <arnel.lenteria@gmail.com>
2023-01-08 13:13:53 +0800

    fix mute/unmute btn on no audio file

--- app/components/mute-button/muteButton.js
+++ app/components/mute-button/muteButton.js
@@ -1,24 +1,27 @@
-define(['knockout', 'sounds', 'app/utils/mutedCookie', 'text!app/components/mute-button/mute-button.html'], function(ko, sounds, muted, tpl){
+define(['knockout', 'sounds', 'app/utils/mutedCookie', 'text!app/components/mute-button/mute-button.html'], function(ko, sounds, muted, tpl) {
 
   function vm() {
     var self = this;
 
-    self.is_muted = ko.observable(muted.getMutedBoolean() || false)
+    self.is_muted = ko.observable(muted.getMutedBoolean() || false);
 
-    self.muteBtn = function(){
-      muted.setMutedCookie("muted");
-      sounds.background.pause();
-      self.is_muted(muted.getMutedBoolean())
-    }
-    self.unMuteBtn = function(){
-      muted.setMutedCookie("unmute")
-      sounds.background.play();
-      self.is_muted(muted.getMutedBoolean())
-    }
+    self.muteBtn = function() {
+      muted.setMutedCookie('muted');
+      if (sounds.background) {
+        sounds.background.pause();
+      }
+      self.is_muted(muted.getMutedBoolean());
+    };
+    self.unMuteBtn = function() {
+      muted.setMutedCookie('unmute');
+      if (sounds.background) {
+        sounds.background.play();
+      }
+      self.is_muted(muted.getMutedBoolean());
+    };
   }
   ko.components.register('mute-button', {
     viewModel: vm,
     template: tpl
-  })
-
-})
+  });
+});
