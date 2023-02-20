1cc8f6c81760b51d9e9ea6b277e18a8f41d5d254
Arnel Lenteria <arnel.lenteria@gmail.com>
2023-02-20 13:53:28 +0800

    show passcode in receipt page

--- app/pages/receipt-page/ReceiptPageVM.js
+++ app/pages/receipt-page/ReceiptPageVM.js
@@ -4,15 +4,20 @@ define([
   'http',
   'sessions',
   'app/observables/receipt',
+  'app/observables/device',
+  'modal',
   'app/components/voucher-form/VoucherForm',
   'app/bindings/clipboard'
-], function(ko, rootVM, http, sessions, receipt) {
+], function(ko, rootVM, http, sessions, receipt, device, modal) {
   return function ReceiptPage() {
     var self = this;
     self.receipt = receipt;
     self.sessions = sessions;
     self.copied = ko.observable(false);
+    self.enablePasscode = ko.observable(false);
     self.connecting = ko.observable(false);
+    self.device = device
+
     self.koDescendantsComplete = function () {
       rootVM.showingStatusNav(false);
       rootVM.showingBanners(false);
@@ -20,10 +25,21 @@ define([
       if (!sessions.hasRunning() && receipt.sessionId()) {
         self.connect();
       }
+
+      http.securityConfig(function(err, data) {
+        if(err) return self.enablePasscode(false);
+        self.enablePasscode(data.enable_passcode);
+      });
     };
     self.afterCopy = function() {
       self.copied(true);
     };
+    self.editPasscode = function () {
+      modal.show('passcode-modal', {onClose: function () {
+        device.fetch(function () {})
+      }});
+    };
+
     self.connect = function() {
       self.connecting(true);
       http.startSession(receipt.sessionId(), function(err) {
