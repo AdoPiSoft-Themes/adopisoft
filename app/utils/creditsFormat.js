bbf1c5a4c13548dd2d57ba791a646131063ea64e
Arnel Lenteria <arnel.lenteria@gmail.com>
2023-06-26 13:35:31 +0800

    cleanup data format

--- app/utils/creditsFormat.js
+++ app/utils/creditsFormat.js
@@ -4,12 +4,10 @@ define(['./shortSecondsFormat', './formatBytes'], function (formatseconds, forma
     var type = item.type;
 
     if (type === 'time') return formatseconds(item.seconds);
-    else if (type === 'data') return formatBytes(item.megabytes);
+    else if (type === 'data') return formatBytes(item.megabytes) + '/' + formatBytes(item.data_mb);
     else if (type === 'time_or_data') return formatseconds(item.seconds) + '/' + formatBytes(item.megabytes);
     else {
-      return item.data_consumption_mb
-        ? formatBytes(item.data_consumption_mb)
-        : 'subscription';
+      return 'Subscription'
     }
   };
 });
